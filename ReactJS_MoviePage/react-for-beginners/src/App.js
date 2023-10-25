a906530c33cc8f19961087130bdc14775f568671
GuDahoon <iamkdh1@naver.com>
2023-10-25 14:50:13 +0900

    studying useEffect

--- ReactJS_MoviePage/react-for-beginners/src/App.js
+++ ReactJS_MoviePage/react-for-beginners/src/App.js
@@ -1,32 +1,22 @@
-import Button from "./Button"
-import styles from "./App.module.css"
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("")
-  const addCounter = () => {
-    setValue((prev) => prev + 1)
-  }
-  const onChange = (event) => {
-    setKeyword(event.target.value)
-  }
+function Hello() {
   useEffect(() => {
-    console.log("I run only once.")
+    console.log("hi :)")
+    return () => console.log("bye :(")
   }, [])
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.")
-  }, [keyword])
-  useEffect(() => {
-    console.log("I run when 'counter' changes.")
-  }, [counter])
+  return <h1>Hello</h1>
+}
+
+function App() {
+  const [showing, setShowing] = useState(false)
+  const onClick = () => {
+    setShowing(prev => !prev)
+  }
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!</h1>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
-      <h2>{counter}</h2>
-      <button onClick={addCounter}>Click Me!</button>
-      <Button text={"Click Me"} />
+      {showing ? <Hello/> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>    
     </div>
   );
 }
