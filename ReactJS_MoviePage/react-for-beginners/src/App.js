6bc86abc040fc25c538b8ce542d34a6e1c5a2b36
GuDahoon <iamkdh1@naver.com>
2023-10-25 12:15:35 +0900

    studying how to use 'useEffect'

--- ReactJS_MoviePage/react-for-beginners/src/App.js
+++ ReactJS_MoviePage/react-for-beginners/src/App.js
@@ -1,11 +1,32 @@
 import Button from "./Button"
 import styles from "./App.module.css"
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("")
+  const addCounter = () => {
+    setValue((prev) => prev + 1)
+  }
+  const onChange = (event) => {
+    setKeyword(event.target.value)
+  }
+  useEffect(() => {
+    console.log("I run only once.")
+  }, [])
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.")
+  }, [keyword])
+  useEffect(() => {
+    console.log("I run when 'counter' changes.")
+  }, [counter])
   return (
     <div>
       <h1 className={styles.title}>Welcome Back!</h1>
-      <Button text={"This is Button"} />
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
+      <h2>{counter}</h2>
+      <button onClick={addCounter}>Click Me!</button>
+      <Button text={"Click Me"} />
     </div>
   );
 }
