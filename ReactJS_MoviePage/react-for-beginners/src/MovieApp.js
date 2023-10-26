abe32230df416642751063fa08fc16f7f270e9b1
GuDahoon <iamkdh1@naver.com>
2023-10-26 15:18:59 +0900

    add navigation

--- ReactJS_MoviePage/react-for-beginners/src/MovieApp.js
+++ ReactJS_MoviePage/react-for-beginners/src/MovieApp.js
@@ -2,12 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 import "./App.module.css";
+import Navigation from "./components/Navigation";
 
 function App() {
     return (
         <Router>
+            <Navigation />
+            <br/>
+            <br/>
+            <br/>
             <Routes>
-                <Route path="/abot-us" element={<h1>Hello</h1>} />
+                <Route path="/about-us" element={<h1>Hello</h1>} />
                 <Route path="/movie/:id" element={<Detail />} />
                 <Route path="/" element={<Home />} />
             </Routes>
