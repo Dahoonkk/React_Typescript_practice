105bb007c3844b3c9d14d959b9dddcde3bf05695
GuDahoon <iamkdh1@naver.com>
2023-11-15 17:26:37 +0900

    update Hooks(useFadeIn, useNetwork)

---
+++ Studying_React_Hooks/studying-hooks/src/useFadeIn.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./styles.css";
+
+const useFadeIn = (duration = 1, delay = 0) => {
+  if (typeof duration !== "number" || typeof delay !== "number") {
+    return;
+  }
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      const { current } = element;
+      current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
+      current.style.opacity = 1;
+    }
+  }, []);
+  return { ref: element, style: { opacity: 0 } };
+};
+
+export default function App() {
+  const fadeInH1 = useFadeIn(2, 2);
+  const fadeInP = useFadeIn(5, 5);
+  return (
+    <div className="App">
+      <h1 {...fadeInH1}>Hello</h1>
+      <p {...fadeInP}>Nice to meet you!</p>
+    </div>
+  );
+}
