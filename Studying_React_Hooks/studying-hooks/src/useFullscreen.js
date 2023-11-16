c506bdc940bdf17120d4dc91d51b484902adeb4a
GuDahoon <iamkdh1@naver.com>
2023-11-16 14:49:17 +0900

    update Hooks(useScroll, useFullscreen)

---
+++ Studying_React_Hooks/studying-hooks/src/useFullscreen.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./styles.css";
+
+const useFullscreen = (callback) => {
+  const element = useRef();
+  const triggerFull = () => {
+    if (element.current) {
+      element.current.webkitRequestFullscreen();
+      if (callback && typeof callback === "function") {
+        callback(true);
+      }
+    }
+  };
+  const exitFull = () => {
+    document.exitFullscreen();
+    if (callback && typeof callback === "function") {
+      callback(false);
+    }
+  };
+  return { element, triggerFull, exitFull };
+};
+
+export default function App() {
+  const onFullS = (isFull) => {
+    console.log(isFull ? " We are full" : "We are small");
+  };
+  const { element, triggerFull, exitFull } = useFullscreen(onFullS);
+  return (
+    <div>
+      <h1>Hello</h1>
+      <div ref={element}>
+        <img src="https://i.ibb.co/R6RwNxx/grape.jpg" alt="grape" width="250" />
+        <button onClick={exitFull}>Exit fullscreen</button>
+      </div>
+      <button onClick={triggerFull}>Make fullscreen</button>
+    </div>
+  );
+}
