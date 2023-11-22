25504fe083034f4945e83e201c87a1db500fa68a
GuDahoon <iamkdh1@naver.com>
2023-11-22 16:19:31 +0900

    update

--- board_practice/src/App.jsx
+++ board_practice/src/App.jsx
@@ -1,9 +1,41 @@
+import { createBrowserRouter } from 'react-router-dom'
 import './App.css'
+import Layout from './components/layout';
+import { RouterProvider } from 'react-router-dom';
+import Home from './routes/home';
+import Login from './routes/login';
+import CreateAccount from './routes/create-account';
+import React from 'react';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+    ]
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />
+  }
+])
 
 function App() {
 
   return (
-    <h1 class="bg-blue-500 hover:bg-blue-400">Hello!</h1>
+    <>
+      <div className="bg-blue-900 aspect-square ">
+        <RouterProvider router={router} />
+      </div>
+    </>
   )
 }
 
